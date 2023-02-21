8132c51b1c2172bf67bd2a061a77c5a501ed2492
Forairaaaaa <applesyqd@outlook.com>
2023-02-21 18:50:27 +0800

    try better i2s reader

--- MicPDM/EmmaMicPDM.hpp
+++ MicPDM/EmmaMicPDM.hpp
@@ -11,20 +11,14 @@
 #pragma once
 #include "../EmmaConfig.h"
 #if EMMA_MODULE_MIC_PDM
-#include "driver/i2s.h"
+#include "I2S/I2SSampler.h"
 
 
 class EmmaMicPDM {
     private:
-        uint8_t __i2s_port;
-        uint16_t __data;
-        size_t __bytes_read;
-
     public:
         EmmaMicPDM(uint8_t i2sPort, int clkPin, int dataPin)
         {
-            __i2s_port = i2sPort;
-            
             // Set the I2S configuration as PDM and 16bits per sample
             i2s_config_t i2s_config = {
                 .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
@@ -47,25 +41,12 @@ class EmmaMicPDM {
                 .data_in_num = dataPin,
             };
 
-            // Call driver installation function before any I2S R/W operation.
-            ESP_ERROR_CHECK( i2s_driver_install((i2s_port_t)__i2s_port, &i2s_config, 0, NULL) );
-            ESP_ERROR_CHECK( i2s_set_pin((i2s_port_t)__i2s_port, &pin_config) );
-            ESP_ERROR_CHECK( i2s_set_clk((i2s_port_t)__i2s_port, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO) );
+            
         }
         ~EmmaMicPDM() {}
 
 
-        int16_t read()
-        {
-            i2s_read((i2s_port_t)__i2s_port, (void*)&__data, sizeof(__data), &__bytes_read, 100);
-            return __data;
-        }
 
-
-        inline esp_err_t readBuffer(void* buffer, size_t bufferSize, size_t* bytesRead, TickType_t ticks2Wait)
-        {
-            return i2s_read((i2s_port_t)__i2s_port, buffer, bufferSize, bytesRead, ticks2Wait);
-        }
         
 };
 
