639fe58b4f71fd1ceb7911fe1c9362cac174ab88
Forairaaaaa <applesyqd@outlook.com>
2023-03-04 11:00:03 +0800

    update pin control

--- Speaker/EmmaSpeaker.hpp
+++ Speaker/EmmaSpeaker.hpp
@@ -27,19 +27,18 @@ class EmmaSpeaker : public m5::Speaker_Class {
             cfg.pin_ws = EMMA_SPEAKER_LRCLK_PIN;
             config(cfg);
             begin();
+
+            enable();
         }
         
         /* For hardware enable pin if exist */
-        inline void powerOn()
-        {
-            gpio_reset_pin((gpio_num_t)EMMA_SPEAKER_ENABLE_PIN);
-            gpio_set_direction((gpio_num_t)EMMA_SPEAKER_ENABLE_PIN, GPIO_MODE_DISABLE);
-        }
-        inline void powerOff()
+        inline void enable() { setCtrlPin(1); }
+        inline void disable() { setCtrlPin(0); }
+        inline void setCtrlPin(uint8_t level)
         {
             gpio_reset_pin((gpio_num_t)EMMA_SPEAKER_ENABLE_PIN);
             gpio_set_direction((gpio_num_t)EMMA_SPEAKER_ENABLE_PIN, GPIO_MODE_OUTPUT_OD);
-            gpio_set_level((gpio_num_t)EMMA_SPEAKER_ENABLE_PIN, 0);
+            gpio_set_level((gpio_num_t)EMMA_SPEAKER_ENABLE_PIN, level);
         }
 };
 
